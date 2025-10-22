6c0841c63471c3e9ca47e63705c54ef386c0e435
Caetano-dev <pcaetanop@protonmail.com>
2025-10-22 17:43:40 -0300

    Add dict and working logic

--- tcpServerIterativo.py
+++ tcpServerIterativo.py
@@ -8,6 +8,19 @@ tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp.bind(orig)
 tcp.listen(1)
 
+palavras = {
+    'rede': {'en': 'network', 'es': 'red'},
+    'roteador': {'en': 'router', 'es': 'enrutador'},
+    'comutador': {'en': 'switch', 'es': 'conmutador'},
+    'conexão': {'en': 'connection', 'es': 'conexión'},
+    'congestionamento': {'en': 'congestion', 'es': 'congestión'},
+    'pacote': {'en': 'packet', 'es': 'paquete'},
+    'latência': {'en': 'latency', 'es': 'latencia'},
+    'propagação': {'en': 'propagation', 'es': 'propagación'},
+    'privacidade': {'en': 'privacy', 'es': 'privacidad'},
+    'criptografia': {'en': 'encryption', 'es': 'cifrado'}
+}
+
 while True:
     con, cliente = tcp.accept()
     print("Conectado com %s" % str(cliente))
@@ -17,7 +30,16 @@ while True:
         if not msg:
             break
         print(cliente, str(msg, encoding='utf-8'))
-        resposta = "SERVER SIDE: " + str.upper(str(msg, encoding='utf-8'))
+        
+        try:
+            palavra, idioma = str(msg, encoding='utf-8').split(':')
+        except ValueError:
+            palavra, idioma = None, None
+        resposta = "SERVER SIDE: Palavra nao encontrada.\nO formato da mensagem deve ser 'palavra:en/es'.\nAs palavras disponiveis são: " + ', '.join(palavras.keys())
+        
+        if palavra in palavras and idioma in palavras[palavra]:
+            resposta = "SERVER SIDE: " + palavras[palavra][idioma]
+            
         con.send(resposta.encode('utf-8'))
 
     print(str.format("Finalizando conexao do cliente {}", cliente))
